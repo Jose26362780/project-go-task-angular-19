57f38b3409d84c82cd26a5e69076dc8dd3f2e858
Jose Martinez <juniorjose1925@gmail.com>
2026-04-02 22:02:03 -0300

    corrigindo a estrutura do modal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
-
 
 @Component({
   selector: 'app-root',
