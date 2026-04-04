e4d3e6ef88182586087cec1ff964ff11c19518c3
Jose Martinez <juniorjose1925@gmail.com>
2026-04-04 19:00:16 -0300

    criando a interfacxe

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { ITask } from '../interfaces/task.interface';
 
 @Injectable({
   providedIn: 'root',
@@ -7,16 +8,16 @@ import { BehaviorSubject } from 'rxjs';
 export class TaskService {
   //Tarefas em a Fazer
 
-  private todoTasks$ = new BehaviorSubject<any[]>([]);
+  private todoTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly todoTasks = this.todoTasks$.asObservable();
 
   // Tarefas em Fazendo
 
-  private doingTasks$ = new BehaviorSubject<any[]>([]);
+  private doingTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly doingTasks = this.todoTasks$.asObservable();
 
   // Tarefas em Concluido
 
-  private doneTasks$ = new BehaviorSubject<any[]>([]);
+  private doneTasks$ = new BehaviorSubject<ITask[]>([]);
   readonly doneTasks = this.todoTasks$.asObservable();
 }
