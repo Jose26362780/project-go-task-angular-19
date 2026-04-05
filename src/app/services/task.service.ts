3ae495085fdde1eb2dbfca3cf3b431f5ff8ade45
Jose Martinez <juniorjose1925@gmail.com>
2026-04-05 18:57:36 -0300

    corrigindo o problema de mutação de lista de tarefas

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, map } from 'rxjs';
 import { ITask } from '../interfaces/task.interface';
 import { ITaskFormControls } from '../interfaces/task-form-controls.interface';
 import { generateUniqueIdWithTimestamp } from '../utils/generate-unique-id-with-timestamp';
@@ -12,17 +12,23 @@ export class TaskService {
   //Tarefas em a Fazer
 
   private todoTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly todoTasks = this.todoTasks$.asObservable();
+  readonly todoTasks = this.todoTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   // Tarefas em Fazendo
 
   private doingTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doingTasks = this.todoTasks$.asObservable();
+  readonly doingTasks = this.doingTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   // Tarefas em Concluido
 
   private doneTasks$ = new BehaviorSubject<ITask[]>([]);
-  readonly doneTasks = this.todoTasks$.asObservable();
+  readonly doneTasks = this.doneTasks$
+    .asObservable()
+    .pipe(map((tasks) => structuredClone(tasks)));
 
   addTask(taskInfos: ITaskFormControls) {
     const newTask: ITask = {
@@ -35,7 +41,9 @@ export class TaskService {
     const currentList = this.todoTasks$.value;
 
     this.todoTasks$.next([...currentList, newTask]);
+  }
 
-
+  carregarListaAtualDeTodos() {
+    console.log('Lista atual TODOS: ', this.todoTasks$.value);
   }
 }
